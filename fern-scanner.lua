a3e348a0e035bbccb2beb6cc75d48c9a8763530e
t8rt0ts <168775188+t8rt0ts@users.noreply.github.com>
2024-05-02 16:04:07 -0700

    Add .dat reading

--- fern-scanner.lua
+++ fern-scanner.lua
@@ -31,3 +31,39 @@ local function numToBiome(num)
     end
     error("No biome found")
 end
+--Dat files contain bytes
+--cX,cZ: number 0-127 telling the chunkX and chunkZ position within the region of the .dat
+--Bytes in .dat file:
+--0 = no chunk data at position
+--1-254 = literal numbers 1-254 (used with the biomes.json metadata path)
+--255 = do 255 + the next byte (recursively if the next byte is also 255)
+local function readDat(path,cX,cZ)
+    if type("path") ~= "string" then error("No path provided") end
+    if not fs.exists(path) then
+        local file = fs.open(path,"w+")
+        file.write(string.rep(string.char(0),16384))
+        file.close()
+        return 0
+    end
+    if tonumber(cX) and tonumber(cZ) then
+        local file = fs.open(path,"rb")
+        local index = (tonumber(cX)*128) + tonumber(cZ)
+        local str = file.read(index) --If there are no 255s, this is all the data until the data we want to read
+        str,matches = str:gsub(string.char(255),"")
+        while matches > 0 do --eating up extra bytes to account for the extra bytes added by each 255
+            if file.read() ~= 255 then matches = matches - 1 end --If 255, keep going
+        end
+        local num,char = 0,0
+        repeat --read until not hitting 255
+            char = file.read()
+            num = num * 255 + char
+        until char ~= 255
+        file.close()
+        return num
+    end
+    --If not specific chunk, then return the entire string
+    local file = fs.open(path,"rb")
+    local data = file.readAll()
+    file.close()
+    return data
+end
